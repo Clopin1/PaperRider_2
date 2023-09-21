50b1e6ffd2b57248d1a8769f159e35bd4a49d431
Clopin <urubudopix974@gmail.com>
2023-09-21 15:09:06 -0300

    uuff

--- PaperRider 2.0/Assets/Assets/Scripts/GameOver.cs
+++ PaperRider 2.0/Assets/Assets/Scripts/GameOver.cs
@@ -2,14 +2,34 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
+    public int health;
+
+    public void Start()
+    {
+        health = 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("LifePowerUp"))
+        {
+            health++;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (collision.transform.tag == "Obstacle")
         {
-            PlayerManagement.isGameOver = true;
-            gameObject.SetActive(false);
-            Time.timeScale = 0f;
-            
+            health--;
+
+            if (health <= 0) 
+            {
+                PlayerManagement.isGameOver = true;
+                gameObject.SetActive(false);
+                Time.timeScale = 0f;
+            }
         }
     }    
 }
